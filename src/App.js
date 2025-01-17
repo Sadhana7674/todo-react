a5000b1cb49060ec229f6f4ff71ed0a9995b7afe
Sadhana7674 <sadhanamanda7@gmail.com>
2025-01-17 10:35:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Home from './pages/Home';
+import './style.css'
+function App() {
+  return (
+    <>
+<h2>TO-DO APP</h2>
+
+   < Home/>
+
+</>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+export default function Home() {
+    return (
+      <>
+  <form>
+    <input type='text'></input>
+    <button>ADD TASK</button>
+  </form>
+  </>
+    );
+  }
+  
+ 
+  
